6e24e26776f24b8ebe1065c9bb18a1b8feefc109
Jisse Meruma <jissemeruma@gmail.com>
2023-01-25 14:41:24 +0100

    back_up

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jmeruma <jmeruma@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 20:43:48 by jmeruma           #+#    #+#             */
-/*   Updated: 2023/01/25 14:06:47 by jmeruma          ###   ########.fr       */
+/*   Updated: 2023/01/25 14:19:27 by jmeruma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,33 @@ void	clean_error(void)
 int	main(int argc, char *argv[], char *envp[])
 {
 	pid_t	id;
+	int		pipes[2]; 
+	// 0 = read // 
+	// 1 = write //
+	int		fd;
 	int		counter;
 	int 	i;
-	t_pipe *pipe;
 	
 	id = 1;
 	counter = 0;
 	i = 1;
+	
 	if (argc < 4)
 		clean_error();
+	pipe(pipes);
 	while (counter < argc - 3 && id != 0)
 	{
 		if (id != 0)
 			id = fork();
 		i++;
 		counter++;
+	}
+	if (id != 0)
+	{
+		fd = open(argv[1], O_RDONLY);
+		if (fd == -1)
+			clean_error();
+		
 	}
 	if (id == 0)
 		commands(envp, argv[i]);
