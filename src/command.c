ca2197f9c8d5cbd66394e1bd3fc20c3973b099fb
Jisse Meruma <jissemeruma@gmail.com>
2023-01-27 11:06:16 +0100

    changed some inconvienience

--- src/command.c
+++ src/command.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   command.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmeruma <jmeruma@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jisse <jisse@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 22:43:22 by jmeruma           #+#    #+#             */
-/*   Updated: 2023/01/25 14:14:52 by jmeruma          ###   ########.fr       */
+/*   Updated: 2023/01/26 12:51:50 by jisse            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,35 @@
 char	**valid_command(char *path, char *argument, char *command)
 {
 	int		i;
-	int		commands;
 	char	**arg_argv;
 	char	**paths;
 
 	i = 0;
-	commands = 0;
 	arg_argv = ft_split(argument, ' ');
 	paths = ft_split(path, ':');
 	while (1)
 	{
 		command = ft_strjoin(paths[i], "/");
-		command = ft_strjoin(command, arg_argv[0]);
+		command = ft_strjoin_free(command, arg_argv[0]);
+		//ft_printf("1=%s\n", command);
+		printf("%s\n", command);
 		if (access(command, F_OK | X_OK) == 0)
+		{
+			printf("COMMAND = %s\n", command);
+			ft_2dfree(paths);
+			free(command);
 			return (arg_argv);
+		}
 		free(command);
 		i++;
 		if (paths[i] == '\0')
+		{
+			ft_2dfree(paths);
+			ft_2dfree(arg_argv);
 			clean_error();
+		}
 	}
-	ft_2dfree(paths);
+	
 }
 
 void	commands(char *envp[], char *argument)
